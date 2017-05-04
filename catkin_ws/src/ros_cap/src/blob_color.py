dbc19e4d52bec549b5d0fcad1f6615fff1f08067
Alejandro Luzanto <janoluzanto1@gmail.com>
2017-05-04 17:50:47 -0300

    codigo no completo

--- catkin_ws/src/ros_cap/src/blob_color.py
+++ catkin_ws/src/ros_cap/src/blob_color.py
@@ -18,8 +18,8 @@ lower_blue = np.array([110,50,50])
 upper_blue = np.array([130,255,255])
 lower_red = np.array([0,0,0])
 upper_red = np.array([0,0,0])
-lower_yellow = np.array([0,0,0])
-upper_yellow = np.array([0,0,0])
+lower_yellow = np.array([25,170,50])
+upper_yellow = np.array([30,255,255])
 
 
 class BlobColor():
@@ -28,25 +28,31 @@ class BlobColor():
 
 
         #Subscribirce al topico "/duckiebot/camera_node/image/raw"
-        self.image_subscriber = None 
-
+        self.image_subscriber = rospy.Subscriber('/duckiebot/camera_node/image/raw', Image, self._process_image)
+        self.base_pub = rospy.Publisher('/duckiebot/segment_node/image', Image, queue_size=1)
+        self.base_pub_2 = rospy.Publisher('/duckiebot/geometry_msgs/Punto', Point, queue_size=1)
         #Clase necesaria para transformar el tipo de imagen
         self.bridge = CvBridge()
 
         #Ultima imagen adquirida
         self.cv_image = Image()
 
-        self.min_area = 10
+        self.min_area = 20
 
 
 
     def _process_image(self,img):
         #Se cambiar mensage tipo ros a imagen opencv
+        try:
+            self.cv_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
+        except CvBridgeError as e:
+            print(e)
 
         #Se deja en frame la imagen actual
         frame = self.cv_image
 
         #Cambiar tipo de color de BGR a HSV
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         # Filtrar colores de la imagen en el rango utilizando 
         mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
@@ -58,26 +64,37 @@ class BlobColor():
         kernel = np.ones((5,5),np.uint8)
 
         #Operacion morfologica erode
-        #img_out = cv2.erode(img, kernel, iterations = 1)
+        img_out = cv2.erode(mask, kernel, iterations = 1)
         
         #Operacion morfologica dilate
-        #img_out = cv2.dilate(img, kernel, iterations = 1)
+        img_out = cv2.dilate(img_out, kernel, iterations = 1)
+        img_out = cv2.erode(img_out, kernel, iterations = 1)
+        img_out = cv2.dilate(img_out, kernel, iterations = 1)
 
-        contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
+        image, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        x1,y1,w1,h1 = 0,0,0,0
         for cnt in contours:
             #Obtener rectangulo
-            #x,y,w,h = cv2.boundingRect(cnt)
+            x,y,w,h = cv2.boundingRect(cnt)
+            
 
             #Filtrar por area minima
             if w*h > self.min_area:
 
                 #Dibujar un rectangulo en la imagen
-                #cv2.rectangle(img, (x1,y1), (x2,y2), (0,0,0), 2)
+                cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,0), 2)
+            if w*h > w1*h1:
+                x1,y1,w1,h1 = cv2.boundingRect(cnt)
+                
 
-        #Publicar frame
+        xc, yc= (x1+w1)/2, (y1+h1)/2
+        
+        imagen = self.bridge.cv2_to_imgmsg(frame, "bgr8")
+
+        self.base_pub.publish(imagen)#Publicar frame
 
         #Publicar Point center de mayor tamanio
+        self.base_pub_2.publish(xc,yc,0)
 
 def main():
 
